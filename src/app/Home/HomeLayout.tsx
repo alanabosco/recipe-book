0a2e2c9f72213b2df44171a02bda39f65eb2024a
alanabosco <alana.bosco@gmail.com>
2024-06-26 14:09:09 -0400

    added home page component with some text and connected that to the main page

---
+++ src/app/Home/HomeLayout.tsx
@@ -0,0 +1,9 @@
+export default function HomeLayout(): JSX.Element {
+    return (
+        <div>
+            <h1>
+                The Bosco Recipe Repo
+            </h1>
+        </div>
+    );
+}
